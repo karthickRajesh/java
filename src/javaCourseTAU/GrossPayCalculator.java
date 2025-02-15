3c2f88b4b2089ac123be8db7e4cd6bf94999efa3
karthickRajesh <karthickrajesh.dn@gmail.com>
2025-02-15 19:39:08 +0530

    GrossPayCalculator

---
+++ src/javaCourseTAU/GrossPayCalculator.java
@@ -0,0 +1,24 @@
+package javaCourseTAU;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+
+    public static void main (String[] args){
+        //Get No of Hours
+        System.out.println("Enter The Number of Hours the Employee Worked");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        //Get Rate for an Hour
+        System.out.println("Enter the Hourly rate");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+        //Multiply Both
+        double wage = rate * hours;
+        //Display result
+        System.out.println("The Wage for the employee is $" + wage);
+
+    }
+}
