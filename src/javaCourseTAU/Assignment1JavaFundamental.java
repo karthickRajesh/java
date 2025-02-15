24765de7e7a98d4c1b5a7f644d20b25105652f16
karthickRajesh <karthickrajesh.dn@gmail.com>
2025-02-15 20:25:49 +0530

    Assignment1JavaFundamental

---
+++ src/javaCourseTAU/Assignment1JavaFundamental.java
@@ -0,0 +1,25 @@
+package javaCourseTAU;
+
+import java.util.Scanner;
+
+public class Assignment1JavaFundamental {
+
+    public static void main(String[] args) {
+        //get Adjective from an User
+        System.out.println("Kindly enter an ADJECTIVE ");
+        Scanner scanner = new Scanner(System.in);
+        String adjective = scanner.nextLine();
+
+        // get a Season from the User
+        System.out.println("Kindly enter an SEASON ");
+        String season = scanner.nextLine();
+        scanner.close();
+        //get an whole number
+        System.out.println("Kindly enter aN WHOLE NUMBER ");
+        int number = scanner.nextInt();
+
+        // Display result
+        System.out.println("on a " + adjective + " " + season + " day, I drink a minimum of " + number + " cups of coffee");
+
+    }
+}
